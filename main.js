f2dc6e401ff5c2379a5f24ec0b8210c2a239bd48
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-09-10 19:05:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ document.getElementById("sketch").innerHTML="Sketch to be drawn - "+ Element_of_
 
 timer_counter = 0;
 timer_check = "";
-drawn_sketch = "";
+drawn_sketch = your_sketch;
 answer_holder = "";
 score = 0;
 
@@ -16,6 +16,8 @@ function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
 }
 
 function updateCanvas() {
@@ -29,18 +31,49 @@ function draw() {
         score++;
         document.getElementById("score").innerHTML="Score: " + score;
     }
+   strokeWeight(7);
+   stroke(0);
+        
+   if(mouseIsPressed){
+     line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+    
 }
 
 function check_sketch() {
   timer_counter++ ;
-  document.getElementById("time").innerHTML= "Time: " + timer_counter;
+  document.getElementById("time").innerHTML= "Time: " + Math.round(timer_counter/ 10) + 'sec';
   if(timer_counter >= 1000) {
      timer_counter = 0;
-     timer_check = Completed;
+     timer_check = 1000;
   }
-  if(timer_check=='Completed' || answer_holder=='set') {
+  if(timer_check=='1000' || answer_holder=='set') {
     timer_check = "";
     answer_holder = "";
     updateCanvas();
+    random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+  Element_of_array = quick_draw_data_set[random_no];
+  console.log("Sketch to be drawn = "+ Element_of_array);
+  document.getElementById("sketch").innerHTML="Sketch to be drawn - "+ Element_of_array;
   }
+}
+
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('your_sketch').innerHTML = 'Your Sketch: ' + results[0].label;
+
+    document.getElementById('accuracy').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
 }
